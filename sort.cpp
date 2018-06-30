cad58222d1d5ee358b14dbc6e3c5b153ca33b8b0
iwtbam <1161784945@qq.com>
2018-06-30 17:18:11 +0800

    BubbleSort

--- sort.cpp
+++ sort.cpp
@@ -14,20 +14,32 @@ void Swap(_ElemType& elem1, _ElemType& elem2)
 template<typename ElemType>
 void BubbleSort(std::vector<ElemType>& elems, std::function<bool(ElemType, ElemType)> compare)
 {
-
+    for(int i = 0;i<elems.size()-1 ;i++)
+    {
+        for(int j = 0;j<elems.size()-1-i;j++)
+        {
+            if(compare(elems.at(j),elems.at(j+1)))
+            {
+                Swap(elems.at(j),elems.at(j+1));
+            }
+        }
+    }
 }
 
 bool greater(int left, int right)
 {
-    return true;
+    return left > right;
 }
 
 
 int main()
 {
-    int a = 10, b = 20;
-    Swap(a, b);
-    std::cout<<"a:"<<a<<std::endl;
-    std::cout<<"b:"<<b<<std::endl;
+    std::vector<int> elems = {1,3,5,32,0,535,-19,4543,0,-7};
+    BubbleSort<int>(elems,greater);
+    for(int i = 0;i<elems.size();i++)
+    {
+        std::cout<<elems.at(i)<<std::endl;
+    }
+    std::cout<<std::endl;
     return 0;
 }
