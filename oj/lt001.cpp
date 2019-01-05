b4fe8c97d9b314c28402533c3b651795428eb48d
IWTBAMaster <1161784945@qq.com>
2019-01-05 15:45:35 +0800

    lt001.cpp

--- oj/lt001.cpp
+++ oj/lt001.cpp
@@ -4,8 +4,16 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+
+#define S2
+
+#if defined(S1)
+
+// 暴力 112ms
 
 class Solution {
 public:
@@ -24,7 +32,66 @@ public:
         return vector<int>();
     }
 };
+#elif defined(S2)
 
+// 双指针 8ms
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int len = nums.size();
+        vector<int> res;
+        if(len < 2)
+            return res;
+        
+        vector<pair<int, int>> new_nums;
+        auto cmp = [](pair<int, int> p1, pair<int,int> p2)
+        {
+          return p1.first < p2.first;  
+        };
+        
+        for(int i = 0; i < len ; i++)
+            new_nums.push_back(pair<int, int>(nums[i], i));
+        
+        sort(new_nums.begin(), new_nums.end(), cmp);
+    
+            
+        int l = 0;
+        int r = len - 1;
+        
+        while(l < r)
+        {
+            auto p1 = new_nums[l];
+            auto p2 = new_nums[r];
+            
+            int sum = p1.first + p2.first;
+            if(sum == target)
+            {
+                if(p1.second < p2.second)
+                {
+                    res.push_back(p1.second);
+                    res.push_back(p2.second);
+                }
+                else
+                {
+                    res.push_back(p2.second);
+                    res.push_back(p1.second);
+                }
+                return res;
+            }
+            else if(sum > target)
+                r--;
+            else
+                l++;
+            
+        }
+        return res;
+    }
+};
+
+
+#endif
 
 int main()
 {
