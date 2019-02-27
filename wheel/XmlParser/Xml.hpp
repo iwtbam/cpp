6de4065941737f1979b8ce64d1c55fa7177f09cd
IWTBAMaster <1161784945@qq.com>
2019-02-27 19:36:57 +0800

    XmlParser 1.1

--- wheel/XmlParser/Xml.hpp
+++ wheel/XmlParser/Xml.hpp
@@ -8,6 +8,9 @@
 #include <fstream>
 #include <iterator>
 #include <iostream>
+#include <regex>
+#include "strings/strings.hpp"
+
 
 namespace iwtbam {
 namespace xml {
@@ -34,25 +37,29 @@ namespace xml {
 
     class XmlParser:public UnCopyable
     {
+
     public:
-        static XmlParser& Instance();
+        using vector_xmlnode_t = std::vector<XmlNode>;
+    public:
+        static XmlParser* Instance();
         XmlNode Parse(std::string file_path);
     private:
         XmlParser()=default;
-        XmlNode ParseHelper(std::string content);
-        
+        std::vector<XmlNode> ParseHelper(std::string content);
+    
     public:
+        static const vector_xmlnode_t  XML_NODE_NIL;
         static const std::string LABEL_PATTERN;
         static const std::string ATTRIB_PATTERN;
     };
 
-    const std::string XmlParser::LABEL_PATTERN = "<(.+?) ((?:\\s|.)+?)>((?:\\s|.)*)</\\1\\s*>";
+    const std::string XmlParser::LABEL_PATTERN = "<(.+?)>((?:\\s|.)*)</.+?>";
     const std::string XmlParser::ATTRIB_PATTERN = "(.+?)\\s*=\\s*\"(.+?)\"";
-
-    XmlParser& XmlParser::Instance()
+    const XmlParser::vector_xmlnode_t XmlParser::XML_NODE_NIL = {};
+    XmlParser* XmlParser::Instance()
     {
         static XmlParser instance_;
-        return instance_;
+        return &instance_;
     }
 
     XmlNode  XmlParser::Parse(std::string file_path)
@@ -60,14 +67,41 @@ namespace xml {
         std::ifstream in(file_path);
         std::istreambuf_iterator<char> begin{in};
         std::string content{begin, {}};
-        return node;
+        XmlParser::vector_xmlnode_t nodes = ParseHelper(content);       
+        if(nodes.size())
+            return nodes[0];
+        return {};
     }
 
-    XmlNode ParseHelper(std::string content)
+    XmlParser::vector_xmlnode_t XmlParser::ParseHelper(std::string content)
     {
-        XmlNode node;
-        for(int i = )
-        return {};
+        // std::cout << content << std::endl;
+
+        XmlParser::vector_xmlnode_t children = {};
+        std::regex r(XmlParser::LABEL_PATTERN);
+        for(std::sregex_iterator it(content.begin(), content.end(),r), end_it {}; it!=end_it;it++)
+        {
+            XmlNode node;
+            node.lable = it->str(1);
+            auto attr_content = it->str(2);
+            node.value = it->str(3);
+            std::cout << node.lable << std::endl;
+            std::cout << node.value << std::endl;
+            std::regex r2(iwtbam::xml::XmlParser::ATTRIB_PATTERN);
+            for(std::sregex_iterator attr_it(attr_content.begin(), attr_content.end(),r2), attr_end_it; attr_it!=attr_end_it; attr_it++)
+            {
+                auto key = strings::strip(attr_it->str(1));
+                auto value = strings::strip(attr_it->str(2));
+                node.attributes[key] = value; 
+            }
+            node.children = ParseHelper(node.value);
+            children.push_back(node);
+        }
+
+        if(children.empty())
+            return XmlParser::XML_NODE_NIL;
+        return children;
+
     }
 
 }
--- wheel/XmlParser/main.cpp
+++ wheel/XmlParser/main.cpp
@@ -7,28 +7,23 @@
 
 using namespace std;
 
+using namespace iwtbam;
+
 int main(int argc, char** argv)
 {
-    fstream in("test.txt");
-    std::string content = {istreambuf_iterator<char>{in}, {}};
-    // cout << content << endl;
-
-    string pattern = iwtbam::xml::XmlParser::LABEL_PATTERN;
-    regex r(pattern);
+    auto* parser = xml::XmlParser::Instance();
+    xml::XmlNode root = parser->Parse("test.txt");
 
 
-    using iwtbam::strings::strip;
+    cout << root.lable << endl;
+    cout << root.value << endl;
 
-    for(sregex_iterator it(content.begin(), content.end(),r), end_it {}; it!=end_it;it++)
+    for(xml::XmlNode node:root.children)
     {
-        auto attr_content = it->str(2);
-        regex r2(iwtbam::xml::XmlParser::ATTRIB_PATTERN);
-        for(sregex_iterator attr_it(attr_content.begin(), attr_content.end(),r2), attr_end_it; attr_it!=attr_end_it; attr_it++)
-        {
-            cout << strip(attr_it->str(1)) << endl;
-            cout << strip(attr_it->str(2)) << endl;
-        }
+        cout << node.lable << endl;
     }
+    
+
 
     return 0;
 }
